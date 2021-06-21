c89185a154b3cc5fbb9f8d2a22840665640fe573
fdemirciler <fdemirciler@gmail.com>
2021-06-22 00:51:50 +0200

    x

---
+++ fibonacci.js
@@ -0,0 +1,43 @@
+// Declare an empty array to store numbers
+const acu = new Array();
+
+// Function to sum last two elements
+function sum(a) {
+    return a[0] + a[1]
+}
+
+// Generate sequence
+
+function generateNum(num) {
+
+    for (let i = 0; i < num; i++) {
+
+        if (acu.length in [0, 1]) {
+            acu.push(i);
+
+        } else {
+
+            acu.push(sum(acu.slice(-2)))
+
+        }
+
+    }
+    return acu
+}
+
+const fibonacci = function (number) {
+
+    return generateNum(number)[number]
+};
+
+
+//module.exports = fibonacci;
+
+//const userInput = document.getElementById('number').value;
+
+function showResult(input) {
+    let userInput = parseInt(document.getElementById(input).value);
+    let div = document.getElementById('result');
+    div.innerHTML =  generateNum(userInput);
+};
+
